fbc468be81eae1b3c98a5c04e3310352110f9a44
RafadeCarvalho <rafael.c@aluno.ufop.edu.br>
2022-08-17 08:44:01 -0300

    Third Commit

--- src/GraphAlgorithms/Main.java
+++ src/GraphAlgorithms/Main.java
@@ -19,6 +19,13 @@ public class Main {
         System.out.println("Lowest Degree: " + g1.lowestDegree());
         System.out.println("\nComplement of the current graph: \n" + g1.complement());
 
-
+        Graph g2 = new Graph(3);
+        g2.addEdge(0, 1, 1);
+        g2.addEdge(1, 0, 1);
+        System.out.println("g2 is subGraph? " + g1.subGraph(g2)); // true
+        Graph g3 = new Graph(4);
+        g3.addEdge(1, 3, 1);
+        g3.addEdge(3, 1, 1);
+        System.out.println("g3 is subGraph? " + g1.subGraph(g3)); // false
     }
 }
